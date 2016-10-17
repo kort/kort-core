931b36e61998f20bb34c28ccc54952397d36b601
Andreas Egloff <egloff.andreas@gmail.com>
2016-10-17 14:02:34 +0200

    blank test

--- src/tests/test_db.py
+++ src/tests/test_db.py
@@ -1,10 +1,10 @@
 # content of test_sample.py
 import pytest
 
-import models
-
-
-def test_db():
-    db_session = models.init_db()
-    assert db_session is not None
-
+# import models
+#
+#
+# def test_db():
+#     db_session = models.init_db()
+#     assert db_session is not None
+#
