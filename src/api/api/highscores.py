9895a4eeb2950787fad744605f90e3a9225ef473
Andreas Egloff <egloff.andreas@gmail.com>
2017-04-23 16:05:52 +0200

    added new services

--- src/api/api/highscores.py
+++ src/api/api/highscores.py
@@ -1,2 +1,7 @@
-def get_highscore(limit):
-    return 'highscore'
+import json
+
+def get_highscore(type, limit):
+    with open('data/highscore_'+type+'.json') as json_data:
+        d = json.load(json_data)
+        return d
+    return '{}'
--- src/api/api/missions.py
+++ src/api/api/missions.py
@@ -1,5 +1,10 @@
+import json
+
 def get_mission(mission_id):
     return 'the mission'
 
 def get_missions(lat, lon, radius, limit):
-    return 'the mission'
+    with open('data/missions.json') as json_data:
+        d = json.load(json_data)
+        return d
+    return '{}'
