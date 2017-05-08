ad2ddf23998f31bda07f38879242ba12c784c238
Andreas Egloff <egloff.andreas@gmail.com>
2017-05-08 20:09:38 +0200

    mission service: language param added

--- src/api/api/missions.py
+++ src/api/api/missions.py
@@ -3,7 +3,8 @@ import json
 def get_mission(mission_id):
     return 'the mission'
 
-def get_missions(lat, lon, radius, limit):
+def get_missions(lat, lon, radius, limit, lang):
+    print('get missions for language '+lang)
     with open('data/missions.json') as json_data:
         d = json.load(json_data)
         return d
