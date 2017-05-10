a789fbae02667484f7a61b51c4ad9e220c3c8ad9
Andreas Egloff <egloff.andreas@gmail.com>
2017-05-10 15:03:48 +0200

    achievments localized

--- src/api/api/achievements.py
+++ src/api/api/achievements.py
@@ -1,6 +1,7 @@
 import json
 
-def get_achievements():
+def get_achievements(lang):
+    print('get achievements for language '+lang)
     with open('data/achievements.json') as json_data:
         d = json.load(json_data)
         return d
