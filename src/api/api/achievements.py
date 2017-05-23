51da183e11166310e2e024429d614e949db334ec
Andreas Egloff <egloff.andreas@gmail.com>
2017-05-23 11:50:15 +0200

    fixed bug where not all achievements were fetched

--- src/api/api/achievements.py
+++ src/api/api/achievements.py
@@ -1,23 +1,30 @@
 import json
 import api.models
 import traceback
-from sqlalchemy import or_
+
+from sqlalchemy import desc
+
 
 db_session = api.models.init_db()
 
-def get_achievements(user_id, lang):
 
+def get_achievements(user_id, lang):
     try:
-        all_badges = db_session.query(api.models.Badge, api.models.UserBadge)\
-            .outerjoin(api.models.UserBadge)\
-            .filter(or_(api.models.UserBadge.user_id == user_id, api.models.UserBadge.user_id == None))
+        all_badges = db_session.query(api.models.Badge).order_by(desc(api.models.Badge.sorting)).all()
+
+        all_acquired_badges = db_session.query(api.models.UserBadge) \
+            .filter(api.models.UserBadge.user_id == user_id)
+
+        all_acquired_badges_dict = {}
+        for badge in all_acquired_badges:
+            all_acquired_badges_dict[badge.badge_id] = badge
 
         badges_achieved = []
-        for badge, user_badge in all_badges:
+        for badge in all_badges:
             achievement_date = None
             achieved = False
-            if user_badge:
-                achievement_date = user_badge.create_date
+            if badge.id in all_acquired_badges_dict:
+                achievement_date = all_acquired_badges_dict[badge.id].create_date
                 achieved = True
             badges_achieved.append(badge.dump(language=lang, achieved=achieved, achievementDate=achievement_date))
 
