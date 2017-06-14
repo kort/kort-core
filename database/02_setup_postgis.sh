2f80293c9ef6a653c982cb027efda3a0122ea1fc
Andreas Egloff <egloff.andreas@gmail.com>
2017-06-14 11:42:18 +0200

    removed unused stuff

--- database/02_setup_postgis.sh
+++ database/02_setup_postgis.sh
@@ -33,23 +33,6 @@ if [ -z $TABLE_NAME ] ; then
     TABLE_NAME="errors"
 fi
 
-# # Install postgis 2.1 using apt-get: apt-get install postgresql-9.1-postgis
-# # See here to get latest release: http://linfiniti.com/2012/05/installing-postgis-2-0-on-ubuntu/
-
-# # uninstall extension (in case it was already installed)
-# echo "Uninstall previous installation of PostGIS"
-# psql -d $DB_NAME -f /usr/share/postgresql/9.1/contrib/postgis-2.0/uninstall_postgis.sql 
-
-# # install extension
-# echo "Install PostGIS"
-# psql -d $DB_NAME -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis.sql
-# echo "Install spatial_ref_sys"
-# psql -d $DB_NAME -f /usr/share/postgresql/9.1/contrib/postgis-2.0/spatial_ref_sys.sql
-
-# echo "Grants for PostGIS"
-# psql -d $DB_NAME -c "GRANT ALL ON geometry_columns TO PUBLIC;"
-# psql -d $DB_NAME -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
-# psql -d $DB_NAME -c "GRANT ALL ON geography_columns TO PUBLIC;"
 
 # add geometry to table
 echo "Add geometry column to $SCHEMA_NAME.$TABLE_NAME"
